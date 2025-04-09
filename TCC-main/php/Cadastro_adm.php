24bc30f7a047862310fbba6b517b48e7f6efe958
PedroMota7 <pedromotati3f@gmail.com>
2025-04-09 14:35:11 -0300

    Add files via upload

---
+++ TCC-main/php/Cadastro_adm.php
@@ -0,0 +1,40 @@
+<?php
+
+
+$nome_adm = $_POST['nome'];
+$email_adm = $_POST['email'];
+$cpf_adm = $_POST['cpf'];
+$cnpj_emp = $_POST['cnpj'];
+$senha_adm = $_POST['senha'];
+
+// $senha_hash = password_hash($senha_adm, PASSWORD_BCRYPT);
+// if(isset($_POST["conf"])){
+    //$string = $_POST["conf"];
+    //echo md5($string);}
+
+$abc = mysqli_connect('localhost', 'root', NULL, 'tcc');
+
+if (!$abc) {
+    die('Erro ao se conectar ao banco de dados: ' . mysqli_connect_error());
+}
+
+
+$sql = "INSERT INTO adm (ID, NOME, email, CPF, cnpj, senha)
+        VALUES (NULL, '$nome_adm', '$email_adm', '$cpf_adm', '$cnpj_emp', '$senha_adm')";
+
+$result2 = mysqli_query($abc, $sql);
+
+if ($result2) {
+  
+    sleep(2);
+	header('location:../pages/Login.html');
+}
+    
+ else {
+    echo "Erro ao cadastrar: " . mysqli_error($abc);
+}
+
+mysqli_close($abc);
+?>
+
+
